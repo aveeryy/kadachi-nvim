480b065ed12837d1b443253df04bfcaf2e02f791
Avery <aveeryy@protonmail.com>
2026-07-05 23:29:55 +0200

    keymap: Add system clipboard mappings

--- config/lua/kadachi-nvim/config/keymaps.lua
+++ config/lua/kadachi-nvim/config/keymaps.lua
@@ -20,3 +20,7 @@ vim.keymap.set({ "n" }, "<leader>dn", getDiagnosticJumpFunction(1), { desc = "Go
 
 -- Tools
 vim.keymap.set({ "n" }, "<leader>lg", ":terminal lazygit<CR>", { desc = "Open lazygit" })
+
+-- Clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system's clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system's clipboard" })
