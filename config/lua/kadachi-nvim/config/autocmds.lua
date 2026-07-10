8fec9edc402cf8bc536a6929aa7b57369459a1bf
Avery <aveeryy@protonmail.com>
2026-07-10 13:18:47 +0200

    autocmd: Add indentation configuration for Nix and Lua

--- config/lua/kadachi-nvim/config/autocmds.lua
+++ config/lua/kadachi-nvim/config/autocmds.lua
@@ -1,3 +1,13 @@
+local function setIndentationWidth(pattern, indentation)
+  vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = pattern,
+    callback = function()
+      vim.opt_local.shiftwidth = indentation
+      vim.opt_local.tabstop = indentation
+    end,
+  })
+end
+
 vim.api.nvim_create_autocmd("VimResized", {
   pattern = "*",
   command = "wincmd =",
@@ -18,3 +28,7 @@ vim.api.nvim_create_autocmd({ "TermClose" }, {
   end,
   desc = "Press ENTER when closing a terminal to skip the 'Process exited' text",
 })
+
+-- Indentation
+setIndentationWidth("*.lua", 2)
+setIndentationWidth("*.nix", 2)
